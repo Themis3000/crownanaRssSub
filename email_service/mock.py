def0340deb0b453412726c77eeafff21646c7cac
Themi Megas <Tcm4760@gmail.com>
2025-07-21 12:50:33 -0500

    added logging to mock emails

--- email_service/mock.py
+++ email_service/mock.py
@@ -1,12 +1,25 @@
 from .base import BaseEmail
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class LoggedEmail:
+    to: str
+    subject: str
+    content: str
 
 
 class MockEmail(BaseEmail):
+    def __init__(self):
+        self.email_log: List[LoggedEmail] = []
+
     def send_email(self, to_addr: str, subject: str, content: str):
+        self.email_log.append(LoggedEmail(to=to_addr, subject=subject, content=content))
         email_message = f"""
         Sent email
-        
         TO: {to_addr}
+        SUBJECT: {subject}
         
         {content}
         """
