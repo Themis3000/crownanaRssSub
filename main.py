097af8b0642c9c0aada5fb655d83407728c25a6c
Themi Megas <Tcm4760@gmail.com>
2025-08-04 12:09:16 -0500

    added /signup endpoing, untested.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from datetime import timedelta
 
-from fastapi import FastAPI, Response
+from email_validator import EmailUndeliverableError
+from fastapi import FastAPI, Response, Request
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.exc import IntegrityError
+
 from db import update_db, engine
 from utils import add_subscriber
 from db import QueryManager
@@ -11,13 +15,35 @@ conn.commit()
 conn.close()
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 @app.post("/signup")
-def register_sub(rss_url: str, email: str, notification_period: str):
+def register_sub(rss_url: str, email: str, notification_period: str, request: Request):
     notification_delta = timedelta(days=int(notification_period[:-1]))
-    with QueryManager() as q:
-        add_subscriber(q=q, rss_url=rss_url, sub_email=email, notification_delta=notification_delta)
+    try:
+        with QueryManager() as q:
+            sub, feed = add_subscriber(q=q, rss_url=rss_url, sub_email=email, notification_delta=notification_delta)
+    except IntegrityError:
+        # Should check if they haven't confirmed the subscription yet.
+        # If they haven't, have the ability to resend.
+        return templates.TemplateResponse(request=request,
+                                          name="signup_failure.jinja2",
+                                          context={"error_explanation": "You're already signed up for notifications"
+                                                                        "from this site!"},
+                                          status_code=400)
+    except EmailUndeliverableError:
+        return templates.TemplateResponse(request=request,
+                                          name="signup_failure.jinja2",
+                                          context={"error_explanation": f"The email you submitted {email} seems to"
+                                                                        f"be invalid."},
+                                          status_code=400)
+    return templates.TemplateResponse(request=request,
+                                      name="signup_success.jinja2",
+                                      context={"email": email,
+                                               "period": notification_delta.days,
+                                               "blog_name": feed.feed_name},
+                                      status_code=201)
 
 
 if __name__ == "__main__":
